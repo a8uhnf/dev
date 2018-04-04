8d81784cf2357627847ab20b0a974c03eeb1e147
hanif <hanif@thetigerworks.com>
2018-04-04 21:26:39 +0600

    added-all

--- dl-ml/test.py
+++ dl-ml/test.py
@@ -1,2 +1,13 @@
+import os
 
-print("Hello World!!!")
+path = os.getcwd()
+print "Curretn path:-"
+print path
+
+bezdekIris_data = open(os.path.join(path, "./dl-ml/data/bezdekIris.data") , "r")
+
+lines = bezdekIris_data.readlines()
+
+print(lines)
+for j in lines:
+    print j
